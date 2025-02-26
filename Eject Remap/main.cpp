b222a8f2c6a157e0a25824cade7d1b0f9960c5ad
Daan Vissers <daanvissers@live.nl>
2025-02-26 18:57:42 +0100

    chore: initialize xcode project

---
+++ Eject Remap/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Eject Remap
+//
+//  Created by Daan Vissers on 26/02/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
