866a709925505c2aa38b87cadbd9b2c3fc69ee9f
Daan Vissers <daanvissers@live.nl>
2025-02-26 22:09:15 +0100

    feat: add volume up mapping

--- Eject Remap/main.cpp
+++ Eject Remap/main.cpp
@@ -4,21 +4,30 @@
 //
 //  Created by Daan Vissers on 26/02/2025.
 //
-
 #include <iostream>
 #include <IOKit/hid/IOHIDManager.h>
 #include <CoreFoundation/CoreFoundation.h>
+#include <ApplicationServices/ApplicationServices.h>
 
 // Eject key usage pair
 #define HID_USAGE_CONSUMER_EJECT 0xB8
-#define HID_USAGE_CONSUMER_VOLUME_UP 0xE9
+#define HID_USAGE_CONSUMER_REMAPPED 0xE9
 
 IOHIDManagerRef hidManager;
 
-// Function to send a HID event (simulating Volume Up key press)
+// Increase volume
+void increaseVolume() {
+    std::cout << "Executing AppleScript..." << std::endl;
+    
+    // Run AppleScript command to increase volume
+    system("osascript -e \"set volume output volume (output volume of (get volume settings) + 5)\"");
+}
+
+// Function to send a HID event
 void sendHIDEvent(uint16_t usage) {
-    // Here, we would use CGEvent or another API to send the event
-    std::cout << "Simulating key press: " << std::hex << usage << std::endl;
+    if (usage == 0xE9) { // Volume Up
+        increaseVolume();
+    }
 }
 
 // Callback function for HID events
@@ -28,8 +37,8 @@ void HIDKeyboardCallback(void* context, IOReturn result, void* sender, IOHIDValu
     uint32_t usage = IOHIDElementGetUsage(element);
     
     if (usagePage == 0x0C && usage == HID_USAGE_CONSUMER_EJECT) { // Consumer Page & Eject Key
-        std::cout << "Eject Key Pressed! Remapping to Volume Up...\n";
-        sendHIDEvent(HID_USAGE_CONSUMER_VOLUME_UP);
+        std::cout << "Eject Key Pressed! Remapping...\n";
+        sendHIDEvent(HID_USAGE_CONSUMER_REMAPPED);
     }
 }
 
